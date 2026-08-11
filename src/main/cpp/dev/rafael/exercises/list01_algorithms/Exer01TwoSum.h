3120ceab21477dbcd9f6f2989831350882535c80
RafaelAvilac <rafael_avilac@yahoo.com.br>
2026-08-11 20:52:12 -0300

    .

--- src/main/cpp/dev/rafael/exercises/list01_algorithms/Exer01TwoSum.h
+++ src/main/cpp/dev/rafael/exercises/list01_algorithms/Exer01TwoSum.h
@@ -6,3 +6,28 @@
 #define ED1_ALGORITHMS_EXERCISES_CPP_EXER01TWOSUM_H
 
 #endif //ED1_ALGORITHMS_EXERCISES_CPP_EXER01TWOSUM_H
+
+#include <stdio.h>
+
+int main(void) {
+
+    int n, t, i, j;
+
+    scanf("%d %d", &n, &t);
+
+    int nums[n];
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == t) {
+                printf("%d %d\n", i, j);
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
--- src/main/cpp/dev/rafael/exercises/list01_algorithms/Exer02RemoveDuplicates.h
+++ src/main/cpp/dev/rafael/exercises/list01_algorithms/Exer02RemoveDuplicates.h
@@ -6,3 +6,40 @@
 #define ED1_ALGORITHMS_EXERCISES_CPP_EXER02REMOVEDUPLICATES_H
 
 #endif //ED1_ALGORITHMS_EXERCISES_CPP_EXER02REMOVEDUPLICATES_H
+
+include <stdio.h>
+
+#define MAX 30005
+
+int main(void) {
+
+    int n, i, k;
+
+    scanf("%d", &n);
+
+    int nums[n];
+    for (i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    /* o primeiro elemento sempre e' unico, entao k comeca em 1 */
+    k = 1;
+    for (i = 1; i < n; i++) {
+        /* se o atual e' diferente do ultimo unico gravado, grava ele */
+        if (nums[i] != nums[k - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+        /* se for igual, simplesmente ignora (nao avanca k) */
+    }
+
+    /* saida: quantidade de unicos e depois os k primeiros elementos */
+    printf("%d\n", k);
+    for (i = 0; i < k; i++) {
+        printf("%d", nums[i]);
+        if (i < k - 1) printf(" ");
+    }
+    printf("\n");
+
+    return 0;
+}
